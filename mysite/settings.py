7000debd90f531360dc0041ea870e2af1eac8b66
pranav0n <159932007+pranav0n@users.noreply.github.com>
2024-08-29 10:41:53 +0000

    Edited the redis location to localhost in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',  # Use the appropriate Redis server URL
+        'LOCATION': 'redis://localhost:6379/1',  # Use the appropriate Redis server URL
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
