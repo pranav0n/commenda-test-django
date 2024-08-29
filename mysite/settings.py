7bcfed332667c60bdaf5c3005d6b8bd64c4a4cf6
pranav0n <159932007+pranav0n@users.noreply.github.com>
2024-08-29 10:41:06 +0000

    Added redis packages and redis to settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,6 +87,20 @@ DATABASES = {
     )
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',  # Use the appropriate Redis server URL
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
+# Optional: This is to ensure Django sessions are stored in Redis
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_CACHE_ALIAS = 'default'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
